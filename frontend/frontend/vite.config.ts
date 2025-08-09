bbe9129b348e08465afb9bb857e2223f4de47f34
TankT72 <adstanislawski@hotmail.com>
2025-08-09 19:37:01 +0200

    upgrades to building and deployments

--- frontend/frontend/vite.config.ts
+++ frontend/frontend/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  let outDir = "dist";
+  if (mode === "azure") {
+    outDir = "../../backend/Orders.Api/wwwroot";
+  }
+  return {
+    plugins: [react()],
+    build: {
+      outDir,
+      emptyOutDir: true,
+    },
+  };
+});
